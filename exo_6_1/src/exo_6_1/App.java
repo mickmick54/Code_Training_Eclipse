2c3966379f2840efa2ff89fb6f4a1029f9ef8c9e
Mickaël Colleu <6249403+mickmick54@users.noreply.github.com>
2023-03-02 08:53:11 +0100

    update exo 6

---
+++ exo_6_1/src/exo_6_1/App.java
@@ -0,0 +1,12 @@
+package exo_6_1;
+
+public class App {
+
+	public static void main(String[] args) {
+		int[] tableau = new int[7]; // déclare un tableau de 7 valeurs numériques
+	    for (int i = 0; i < tableau.length; i++) {
+	      tableau[i] = 0; // initialise chaque élément du tableau à zéro
+	    }
+	}
+
+}
---
+++ exo_6_2/src/exo_6_2/App.java
@@ -0,0 +1,14 @@
+package exo_6_2;
+
+public class App {
+
+	public static void main(String[] args) {
+		char[] voyelles = {'a', 'e', 'i', 'o', 'u', 'y'};
+        
+        System.out.println("Les voyelles sont :");
+        for(int i = 0; i < voyelles.length; i++) {
+            System.out.println(voyelles[i]);
+        }
+	}
+
+}
